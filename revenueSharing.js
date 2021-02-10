548ea0514348947f27b0b44d36f8e8cd76863cc3
Nikos Tziralis <ntziralis@gmail.com>
2021-02-10 15:38:21 +0200

    Set env var to use receipt verification

--- revenueSharing.js
+++ revenueSharing.js
@@ -1,35 +1,24 @@
 require('dotenv').config()
 const util = require('util')
 const Web3 = require('web3');
-// const provider = 'wss://mainnet-ws.thundercore.com'
 const provider = `wss://rinkeby.infura.io/ws/v3/${process.env.INFURA_KEY}`
 var web3 = new Web3(provider);
-// const BigNumber = require('bignumber.js');
-
-// const tippingContract = require('./tippingContract')
 const revenueSharingContract = require('./revenueSharingContract')
-// const simpleStorage = require('./simpleStorage')
 
-
-// var contract = new web3.eth.Contract(tippingContract.ABI, tippingContract.address)
 var contract = new web3.eth.Contract(revenueSharingContract.ABI, revenueSharingContract.address)
-// var contract = new web3.eth.Contract(simpleStorage.ABI, simpleStorage.address)
-// console.log(contract)
 
-const _pointer = async () => {
-  // var getValue = await contract.methods.get().call()
+const _pointer = async (useReceiptVerification) => {
   var totalPercentage = await contract.methods.getTotalPercentage().call()
   var randomNum = parseInt(Math.random() * totalPercentage)
   var pointer = await contract.methods.pickPointer(randomNum).call()
   
-  const  receiptVerifierService = 'https://webmonetization.org/api/receipts/'
-  pointer = receiptVerifierService + encodeURIComponent(pointer)
+  if (useReceiptVerification) {
+    const receiptVerifierService = 'https://webmonetization.org/api/receipts/'
+    pointer = receiptVerifierService + encodeURIComponent(pointer)
+  }
   return pointer
-  // let number = new BigNumber(123);
-  // var setValue = await contract.methods.set(number).call()
-  // console.log(setValue)
 }
 
-module.exports = {
-  pointer: _pointer()
+module.exports = (useReceiptVerification) => {
+  return _pointer(useReceiptVerification)
 };
--- server.js
+++ server.js
@@ -7,111 +7,128 @@ const fetch = require("node-fetch");
 var cors = require('cors')
 require('dotenv').config()
 
-const pickPointerSmartContract = require('./revenueSharing')
-
-
 app.set('view engine', 'ejs')
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 app.use(cors())
 
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
+
+const useSmartContract = (process.env.USE_SMART_CONTRACT == 'true') 
+const useReceiptVerification = (process.env.USE_RECEIPT_VERIFICATION == 'true')
+const pickPointerSmartContract = require('./revenueSharing')
+
 // Use smartContract or not to pick payment pointers.
-const useSmartContract = process.env.USE_SMART_CONTRACT
 if (useSmartContract) {
-    console.log('Using smart contract for revenue sharing')
+  console.log('Using smart contract for revenue sharing')
 } else {
-    console.log('Not using smart contract for revenue sharing')
+  console.log('Not using smart contract for revenue sharing')
 }
 
-app.post('/verifyReceipt', async (req, res) => {
-    console.log('Receipt verification active')
+// Use receipt verification or not.
+if (useReceiptVerification) {
+  console.log('Receipt verification: Activated')
+} else {
+  console.log('Receipt verification: Deactivated')
+}
 
-    const resp = await fetch('https://webmonetization.org/api/receipts/verify', {
-        method: 'POST',
-        body: req.body.receipt
-    })
-    var response = resp
-    try {
-        // const { amount } = await resp.json()
-        const response = await resp.json()
-        console.log('Received ' + response)
-        res.send({message: 'ok', response: response})
-    } catch (error) {
-        console.log('RESPONSE: ', response)
-        console.log('ERROR: ', error)
-        res.status(400).send(error)
-    }
-    // backend logic for new paid amount
-    
+
+app.post('/verifyReceipt', async (req, res) => {
+  console.log('Receipt verification active')
+
+  const resp = await fetch('https://webmonetization.org/api/receipts/verify', {
+    method: 'POST',
+    body: req.body.receipt
+  })
+  var response = resp
+  try {
+    const { amount } = await resp.json()
+    console.log('Received ' + amount)
+    res.send({ message: 'ok', data: { received: amount } })
+  } catch (error) {
+    res.status(400).send(error)
+  }
+  // backend logic for new paid amount
+
 })
 
 
 // Define your revenue share here.
 // If these weights add to 100 then they represent the percentage each pointer gets.
 const pointers = {
-    // // Euro
-    // '$ilp.uphold.com/BzR2nf3Yiyak': 20,
-    'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FBzR2nf3Yiyak': 20, //receipt
-    // // USD
-    // '$ilp.uphold.com/pNAg9yAgHXZD': 40,
-    'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FpNAg9yAgHXZD': 40, //receipt
-    // // Pounds
-    // '$ilp.uphold.com/MXhL7Ub9XQeQ': 10,
-    'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FMXhL7Ub9XQeQ': 10, //receipt
-    // // Bitcoin
-    // '$ilp.uphold.com/3eYm4dKzX4hU': 30,
-    // XRP - receipt
-    'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FfH9eZNdYQ64F': 30
+  // Euro
+  '$ilp.uphold.com/BzR2nf3Yiyak': 20,
+  // USD
+  '$ilp.uphold.com/pNAg9yAgHXZD': 40,
+  // Pounds
+  '$ilp.uphold.com/MXhL7Ub9XQeQ': 10,
+  // Bitcoin
+  '$ilp.uphold.com/3eYm4dKzX4hU': 10,
+  // XRP
+  '$ilp.uphold.com/fH9eZNdYQ64F': 20,
+}
+
+const pointersForReceipt = {
+  // Euro
+  'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FBzR2nf3Yiyak': 20, //receipt
+  // USD
+  'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FpNAg9yAgHXZD': 40, //receipt
+  // Pounds
+  'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FMXhL7Ub9XQeQ': 10, //receipt
+  // Bitcoin
+  'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2F3eYm4dKzX4hU': 10, //receipt
+  // XRP
+  'https://webmonetization.org/api/receipts/%24ilp.uphold.com%2FfH9eZNdYQ64F': 20, //receipt
 }
 
 // this is the same `pickPointer()` function implemented in the previous snippet
 function pickPointer() {
-    const sum = Object.values(pointers).reduce((sum, weight) => sum + weight, 0)
-    let choice = Math.random() * sum
+  const sum = Object.values(pointers).reduce((sum, weight) => sum + weight, 0)
+  let choice = Math.random() * sum
 
-    for (const pointer in pointers) {
-        const weight = pointers[pointer]
-        if ((choice -= weight) <= 0) {
-            return pointer
-        }
+  if (useReceiptVerification) {
+    pointers = pointersForReceipt
+  }
+
+  for (const pointer in pointers) {
+    const weight = pointers[pointer]
+    if ((choice -= weight) <= 0) {
+      return pointer
     }
+  }
 }
 
 // NOTE: If you want to have separetly the frontend and the backend (2 instances), you need to use app.get('/') instead of app.use()
 
 app.get('/', async function (req, res, next) {
-    // is this request meant for Web Monetization?
+  // is this request meant for Web Monetization?
 
-    if (req.header('accept').includes('application/spsp4+json')) {
-        console.log('Revenue sharing active')
+  if (req.header('accept').includes('application/spsp4+json')) {
+    console.log('Revenue sharing active')
 
-        // choose our random payment pointer
-        if (useSmartContract) {
-            var pointer = await pickPointerSmartContract.pointer
-        } else {
-            var pointer = pickPointer()
-        }
-
-        console.log('Payment pointer = ', pointer)
-
-        // turn the payment pointer into a URL in accordance with the payment pointer spec
-        // https://paymentpointers.org/
-        const asUrl = new URL(pointer.startsWith('$') ? 'https://' + pointer.substring(1) : pointer)
-        asUrl.pathname = asUrl.pathname === '/' ? '/.well-known/pay' : asUrl.pathname
-
-        // redirect to our chosen payment pointer so they get paid
-        res.redirect(302, asUrl.href)
+    // choose our random payment pointer
+    if (useSmartContract) {
+      var pointer = await pickPointerSmartContract(useReceiptVerification)
     } else {
-        console.log('Revenue sharing not active')
-        // if the request is not for Web Monetization, do nothing
-        next()
+      var pointer = pickPointer()
     }
+
+    console.log('Payment pointer = ', pointer)
+
+    // turn the payment pointer into a URL in accordance with the payment pointer spec
+    // https://paymentpointers.org/
+    const asUrl = new URL(pointer.startsWith('$') ? 'https://' + pointer.substring(1) : pointer)
+    asUrl.pathname = asUrl.pathname === '/' ? '/.well-known/pay' : asUrl.pathname
+
+    // redirect to our chosen payment pointer so they get paid
+    res.redirect(302, asUrl.href)
+  } else {
+    console.log('Revenue sharing not active')
+    // if the request is not for Web Monetization, do nothing
+    next()
+  }
 })
 
-
-const PORT = process.env.PORT || 1337;
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-});
